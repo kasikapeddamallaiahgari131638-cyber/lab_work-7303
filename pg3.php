5b838e4e7cdddaa0c16ee00480baa030354c45dd
kasikapeddamallaiahgari131638-cyber <kasika.peddamallaiahgari131638@marwadiuniversity.ac.in>
2026-06-11 11:56:43 +0530

    Add files via upload

---
+++ pg3.php
@@ -0,0 +1,9 @@
+<?php
+$a= 20;
+$b= 40;
+echo "<h3> Arithematic Operations</h3>";
+echo "Addition". ($a+$b)."<br>";
+echo "Subtraction". ($b-$a)."<br>";
+echo "Multiplcation". ($a*$b)."<br>";
+echo "Divison". ($b/$a)."<br>";
+?>
