a0b5ace1ad1bb133212a03eb7961b5056d8ac26e
kasikapeddamallaiahgari131638-cyber <kasika.peddamallaiahgari131638@marwadiuniversity.ac.in>
2026-06-13 23:45:12 +0530

    Add files via upload

---
+++ pg6.php
@@ -0,0 +1,11 @@
+<?php
+function collegeInfo()
+{
+    echo "College Name: ABC College<br>";
+    echo "Course Name: Bachelor of Computer Applications (BCA)<br>";
+    echo "Academic Year: 2025-2026<br>";
+}
+
+// Function call
+collegeInfo();
+?>
