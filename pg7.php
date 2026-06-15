36be675ac222817b0d56d412c6e4d1be33b16f44
kasikapeddamallaiahgari131638-cyber <kasika.peddamallaiahgari131638@marwadiuniversity.ac.in>
2026-06-15 10:20:27 +0530

    Add files via upload

---
+++ pg7.php
@@ -0,0 +1,7 @@
+function studentDetails(studentName, enrollmentNumber, semester) {
+    console.log("Student Name:", studentName);
+    console.log("Enrollment Number:", enrollmentNumber);
+    console.log("Semester:", semester);
+}
+
+studentDetails("Prasham Parmar", "ENR2026001", 5);
