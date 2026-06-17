efde05eac7d29b0021d8492917accd60f2fe4220
kasikapeddamallaiahgari131638-cyber <kasika.peddamallaiahgari131638@marwadiuniversity.ac.in>
2026-06-17 10:04:32 +0530

    Add files via upload

--- pg9.php
+++ pg9.php
@@ -1,22 +1,12 @@
-#include <stdio.h>
+<?php
 
-float calculatePercentage(int m1, int m2, int m3, int m4, int m5)
-{
-    int total = m1 + m2 + m3 + m4 + m5;
-    float percentage = (total / 500.0) * 100;
-    return percentage;
+function welcomeStudent() {
+    echo "Welcome Student!";
 }
 
-int main()
-{
-    int s1, s2, s3, s4, s5;
-
-    printf("Enter marks of 5 subjects: ");
-    scanf("%d %d %d %d %d", &s1, &s2, &s3, &s4, &s5);
-
-    float result = calculatePercentage(s1, s2, s3, s4, s5);
-
-    printf("Percentage = %.2f%%\n", result);
-
-    return 0;
+if (function_exists('welcomeStudent')) {
+    welcomeStudent(); // Call the function
+} else {
+    echo "Error: Function does not exist.";
 }
+?>
